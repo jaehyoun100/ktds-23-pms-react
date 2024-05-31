63b3746d05608008388946fb6109a4a6d6f5008e
Sang Geol Park <psg02013@gmail.com>
2024-05-31 16:13:01 +0900

    [2024-05-31][박상걸] project-read-me 제작

--- src/components/project/main/MainReadMe.js
+++ src/components/project/main/MainReadMe.js
@@ -1,4 +1,15 @@
 import "../project.css";
-export default function MainReadMe() {
-  return <></>;
+import { FaBookBookmark } from "react-icons/fa6";
+
+export default function MainReadMe({ memo }) {
+  return (
+    <>
+      <div className="display-flex">
+        <FaBookBookmark />
+        <div className="readme">Read Me</div>
+      </div>
+
+      <div className="readme-container">{memo}</div>
+    </>
+  );
 }
