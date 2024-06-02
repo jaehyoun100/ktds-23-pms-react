5549b08dd77d168eb1a0e678b50ec90c19157db6
Sohyun Kim <ksh96611@naver.com>
2024-06-03 08:51:16 +0900

    [2024-06-03][김소현] 프로젝트 뷰 페이지 헤더 작성

--- src/components/project/main/MainHeader.js
+++ src/components/project/main/MainHeader.js
@@ -1,4 +1,22 @@
 import "../project.css";
 export default function MainHeader() {
-  return <></>;
+  return (
+    <div className="header-container">
+      <div className="flex">
+        <h4>
+          <span>담당부서</span> / <span>프로젝트명</span>
+        </h4>
+        <h6>PM : 아무개</h6>
+      </div>
+      <div className="header-menu">
+        <span>참여원관리</span>
+        <span>요구사항</span>
+        <span>이슈관리</span>
+        <span>문답</span>
+        <span>산출물</span>
+        <span>설문</span>
+        <span>후기</span>
+      </div>
+    </div>
+  );
 }
