3b5f4cd054e034afc61dd50b072d310e30a8be1c
Sohyun Kim <ksh96611@naver.com>
2024-06-03 14:31:25 +0900

    [2024-06-03][김소현]프로젝트 리스트 불러오기 구현

--- src/components/project/projectlist/ProjectListApp.js
+++ src/components/project/projectlist/ProjectListApp.js
@@ -1,21 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Button from "../../common/Button/Button";
 import Search from "../../common/search/Search";
+import { useSelector } from "react-redux";
 
 const ProjectListApp = () => {
   const [data, setData] = useState([]);
   const [searchDataCommonCode, setSearchDataCommonCode] = useState();
+
+  const tokenInfo = useSelector((state) => {
+    return {
+      token: state.tokenInfo.token,
+      credentialsExpired: state.tokenInfo.credentialsExpired,
+    };
+  });
   useEffect(() => {
     const getList = async () => {
       const response = await fetch("http://localhost:8080/api/project/search", {
-        headers: {},
+        headers: { Authorization: tokenInfo.token },
         method: "GET",
       });
       const json = await response.json();
-      return json;
+      return json.body;
     };
-    setData(getList());
-  }, []);
+    const getProject = async () => {
+      const run = await getList();
+      setData(run[1]);
+      console.log(run[1]);
+      setSearchDataCommonCode(run[0]);
+    };
+    getProject();
+  }, [tokenInfo.token]);
   const onClickHandler = () => {
     // 프로젝트 생성 api 호출
   };
@@ -25,14 +39,15 @@ const ProjectListApp = () => {
   };
   return (
     <div>
+      <div>{data.projectCount}개의 프로젝트</div>
       <Button onClickHandler={onClickHandler}>생성</Button>
       <Search
         optionList={data.projectCommonCodeList}
         setSelectedData={setSearchDataCommonCode}
         onClickHandler={searchOnClickHandler}
       />
-      {data.projectListVO?.map(() => (
-        <></>
+      {data.projectList?.map((item, idx) => (
+        <div key={idx}>{item.prjName}</div>
       ))}
     </div>
   );
