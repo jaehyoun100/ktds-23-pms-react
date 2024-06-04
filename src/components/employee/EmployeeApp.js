845295ca8977cdfe00399ef7494fefd18ea854ce
수림 <soosoorim@daum.net>
2024-06-04 09:44:05 +0900

    [2024-06-04] [홍수림] 테이블 유틸 공유- EmployeeApp 참고

--- src/components/employee/EmployeeApp.js
+++ src/components/employee/EmployeeApp.js
@@ -1,49 +1,132 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Button, Input, Select } from "antd";
+import Table from "../../utils/Table";
+import { useNavigate } from "react-router-dom";
+import { CustomButton } from "./components/EmployeeView";
 
 export default function EmployeeApp() {
-  const [data, setData] = useState();
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState();
+  const [data, setData] = useState([]);
+  const [selectedList, setSelectedList] = useState();
+  const navigate = useNavigate();
+  // const [loading, setLoading] = useState();
+  // const [error, setError] = useState();
+  const token = true;
+
+  const loadData = useCallback(async () => {
+    await fetch("http://localhost:8080/api/v1/employee", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data.body));
+  }, []);
+
+  // const loadData = useCallback(async () => {
+  //   const response = await fetch("http://localhost:8080/api/v1/employee", {
+  //     method: "GET",
+  //     headers: {
+  //       Authorization: token,
+  //     }
+  //   })
+  //   const json = await response.json();
+  //   setData(json.body);
+  // }, [token]);
 
   useEffect(() => {
-    fetch("/api/employee")
-      .then((response) => {
-        // 응답이 성공적이지 않은 경우 오류 던지기
-        if (!response.ok) {
-          throw new Error("!!!");
-        }
-        // JSON 응답을 JavaScript 객체로 변환
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        // 데이터 상태 업데이트 및 로딩 상태 해제
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        // 오류 상태 업데이트 및 로딩 상태 해제
-        setError(error);
-        setLoading(false);
-      });
-  }, []); // 빈 배열을 전달하여 컴포넌트 마운트 시에만 실행
+    if (token) loadData();
+  }, [token, loadData]);
 
+  const columns = [
+    {
+      title: "사번",
+      dataIndex: "empId",
+      key: "empId",
+      width: "20%",
+    },
+    {
+      title: "사원명",
+      dataIndex: "empName",
+      key: "empName",
+      // render는 테이블에서 dataIndex에서 지정한 데이터를 첫번째 argument로 받고
+      // 해당 row 전체를 두번째 argument로 받는다.
+      // 여기서는 empName이라는 dataIndex로 값을 가져오기때문에, 첫번째 인자인 data 는 empName이다.
+      // 여기서는 사원명을 클릭하면, 사원정보페이지로 이동하는데, 사원정보페이지에서 해당 사원의 정보조회를 위해 empId가 필요하기 때문에
+      // row에서 empId를 추출해 사용한다.
+      // =============
+      // title: "사원명",
+      // key: "empName",
+      // render: (row) => (
+      //   <span style={{ cursor: "pointer" }} onClick={() => navigate(`/employee/view/${row.empId}`)}>
+      //     {row.empName}
+      //   </span>
+      // ),
+      // 위와같이, dataIndex를 지정하지 않는 경우에는 첫번째 인자로 row를 받을 수 있다.
+      render: (data, row) => (
+        <span
+          style={{ cursor: "pointer" }}
+          onClick={() => navigate(`/employee/view/${row.empId}`)}
+        >
+          {data}
+        </span>
+      ),
+    },
+    {
+      title: "부서",
+      dataIndex: ["departmentVO", "deptName"],
+      key: "deptName",
+      width: "20%",
+      // 데이터의 구조가, 하위뎁스로 내려가는 경우 데이터 인덱스는, 위와같이 배열로 입력한다.
+    },
+    {
+      title: "팀",
+      dataIndex: ["teamVO", "tmName"],
+      key: "tmName",
+      width: "20%",
+    },
+
+    {
+      title: "입사 날짜",
+      dataIndex: "hireDt",
+      key: "hireDt",
+      width: "20%",
+    },
+  ];
+
+  // for (let i = 0; i < 100; i++) {
+  //   data.push({
+  //     key: i,
+  //     empId: "empId",
+  //     empName: "empName",
+  //     departmentVO: "departmentVO.deptName",
+  //     teamVO: "teamVO.tmName",
+  //     hireDt: "hireDt",
+  //   });
+  // }
+
+  //검색 필터
+  const filterOptions = [
+    {
+      label: "사번",
+      value: "empId",
+    },
+    {
+      label: "사원명",
+      value: "empName",
+    },
+    // {
+    //   label: "부서명",
+    //   value: "departmentVO", "deptName",
+    // },
+  ];
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>사원ID</th>
-          <th>이름</th>
-          <th>부서</th>
-          <th>팀</th>
-          <th>직무</th>
-        </tr>
-      </thead>
-      <tbody>
-        {/* <tr key={employee.id}>
-          <td>{employee.id}</td>
-        </tr> */}
-      </tbody>
-    </table>
+    <>
+      <Table
+        columns={columns}
+        dataSource={data}
+        rowKey={(dt) => dt.empId}
+        filter
+        filterOptions={filterOptions}
+      />
+      <CustomButton>ddd</CustomButton>
+    </>
   );
 }
---
+++ src/utils/Table.js
@@ -0,0 +1,121 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Table as AntTable, Card, Input, Select } from "antd";
+import { produce } from "immer";
+
+export default function Table({
+  dataSource,
+  filter,
+  filterOptions,
+  columns,
+  pagination,
+  ...props
+}) {
+  const [filterParam, setFilterParam] = useState({ type: "", keyword: "" });
+
+  const tableColumns = useMemo(
+    () => columns.filter((col) => !col.formOnly),
+    [columns]
+  );
+
+  useEffect(() => {
+    // 테이블 마운트(컴포넌트를 DOM에 추가) 시,
+    // filter옵션이 true이고, filterOptions array에 값이 있으면, filter(Select)검색조건 Option을 만들어줌
+    if (filter && filterOptions && filterOptions.length > 0) {
+      setFilterParam(
+        produce((draft) => {
+          draft.type = filterOptions[0].value;
+        })
+      );
+    }
+  }, [filterOptions, filter]);
+
+  // handleInputChange 함수는 대부분의 데이터(1depth)인 경우에 input값 변경될때 업데이트해주도록 쓸 수 있음.
+  // 아래 Select, Input에서 사용하는 예를 보면 됨.
+  const handleInputChange = useCallback(
+    (key) => (e) => {
+      const value = e.target ? e.target.value : e;
+      setFilterParam(
+        produce((draft) => {
+          draft[key] = value;
+        })
+      );
+    },
+    []
+  );
+
+  return (
+    <>
+      {filter && (
+        <Card style={{ marginBottom: 10, padding: 0 }}>
+          <div>
+            <div>
+              <Select
+                options={filterOptions}
+                style={{ width: 150 }}
+                value={filterParam.type}
+                onChange={handleInputChange("type")}
+              />
+
+              <Input
+                value={filterParam?.keyword}
+                onChange={handleInputChange("keyword")}
+              />
+            </div>
+          </div>
+        </Card>
+      )}
+      <AntTable
+        dataSource={
+          dataSource
+            ? dataSource?.filter((dt) =>
+                String(dt[filterParam?.type]).includes(filterParam?.keyword)
+              )
+            : []
+        }
+        pagination={{
+          position: ["bottomCenter"],
+          showSizeChanger: true,
+          ...pagination,
+        }}
+        size="small"
+        columns={tableColumns}
+        {...props}
+      />
+    </>
+  );
+}
+
+/**
+ * 각 App.js에 사용 예시
+ *
+ *   const filterOptions = [
+ *       {
+ *         label: 'ID',
+ *         value: 'id',
+ *       },
+ *       {
+ *         label: '이름',
+ *         value: 'name',
+ *       },
+ *     ];
+ *     위와같은 항목을 filterOptions에 전달하면, 해당 내용으로 필터할 항목이 select에 표시 됨.
+ *
+ *
+ * return (
+    <>
+      <Table
+        columns={tableColumns}
+        dataSource={currencyList}
+        rowKey={(dt) => dt.id}
+        filter
+        filterOptions={filterOptions}
+      />
+    </>
+  );
+ *
+ * rowKey는 data중에서 고유값(dataIndex)을 지정해주어야 함.
+ * 위 예시처럼 {(dt)=> dt.id} 라고 하면 데이터항목중 id라는 값을 기준으로 고유하게 row를 생성해줌.
+ * filter 라고 넣는것은 filter={true} 인데, filter라고만 넣으면 이게 곧 true라는 의미이므로 중복제거하여 filter라고 쓴 것.
+ * filterOptions에 options를 넣어줘야 함.
+ *
+ */
