cf2413e474aa41249aa2eafa52aa4d2b82cf9cf9
seunguuu <youu0208@naver.com>
2024-06-05 17:44:05 +0900

    [2024-06-05][유승훈] 요구사항 작성, 수정 페이지 개선

--- src/http/requirementHttp.js
+++ src/http/requirementHttp.js
@@ -62,3 +62,53 @@ export const loadNameByPrjName = async ({ token, selectedPrjId }) => {
 
   return json;
 };
+
+export const writeRequirement = async (token, formData) => {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/requirement/write`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    }
+  );
+
+  const json = await response.json();
+
+  return json;
+};
+
+export const loadForModifyData = async (token, prjId, rqmId) => {
+  if (!token) {
+    return undefined;
+  }
+  const response = await fetch(
+    `http://localhost:8080/api/v1/requirement/modify?prjId=${prjId}&rqmId=${rqmId}`,
+    {
+      method: "GET",
+      headers: { Authorization: token },
+    }
+  );
+  const json = await response.json();
+
+  return json;
+};
+
+export const modifyRequirement = async (token, rqmId, formData) => {
+  const response = await fetch(
+    `http://localhost:8080/api/v1/requirement/modify/${rqmId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: token,
+      },
+      body: formData,
+    }
+  );
+
+  const json = await response.json();
+
+  return json;
+};
