cb3a110c09b56b728547f50aff6e4998e27f739f
Sang Geol Park <psg02013@gmail.com>
2024-06-05 13:44:20 +0900

    [2024-06-05][박상걸] teammate 생성 ui 기초 제작

--- src/router/routercomponent/projectRouter.js
+++ src/router/routercomponent/projectRouter.js
@@ -1,5 +1,6 @@
 import CreateProjectApp from "../../components/project/createproject/CreateProjectApp";
 import ProjectMain from "../../components/project/main/ProjectMain";
+import TeamMate from "../../components/project/main/teammate/TeamMate";
 import ProjectListApp from "../../components/project/projectlist/ProjectListApp";
 
 const projectRouter = {
@@ -8,6 +9,7 @@ const projectRouter = {
     { index: true, element: <ProjectListApp /> },
     { path: "view", element: <ProjectMain /> },
     { path: "create", element: <CreateProjectApp /> },
+    { path: "manage-teammate", element: <TeamMate /> },
   ],
 };
 export default projectRouter;
