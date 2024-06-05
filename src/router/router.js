dcf9bdca71483b555d28e02016393300cbe8592b
BumJin Jo <jobumjin5500@gmail.com>
2024-06-05 09:26:28 +0900

    [2024-06-05][조범진] 라우터 수정

--- src/router/router.js
+++ src/router/router.js
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../layout/content/Main";
 import NotFoundError from "../components/errors/NotFoundError";
 import SurveyApp from "../components/survey/SurveyApp";
-import EmployeeApp from "../components/employee/EmployeeApp";
 import projectRouter from "./routercomponent/projectRouter";
 import outputRouter from "./routercomponent/outputRouter";
 import requirementRouter from "./routercomponent/requirementRouter";
 import supplyRouter from "./routercomponent/supplyRouter";
 import deptteamRouter from "./routercomponent/deptteamRouter";
-import ReviewApp from "../components/review/ReviewApp";
 import reviewRouter from "./routercomponent/reviewRouter";
+import employeeRouter from "./routercomponent/employeeRouter";
+import memoRouter from "./routercomponent/memoRouter";
 
 export default function RouterAppProvider() {
   const routers = createBrowserRouter([
@@ -24,15 +24,13 @@ export default function RouterAppProvider() {
           element: <SurveyApp />,
         },
         projectRouter,
-        {
-          path: "employee",
-          element: <EmployeeApp />,
-        },
+        employeeRouter,
         requirementRouter,
         outputRouter,
         supplyRouter,
         deptteamRouter,
         reviewRouter,
+        memoRouter,
       ],
     },
   ]);
