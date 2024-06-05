8e6221d01500e965d114853524ccf01ad8be592f
Songhwa Park <hello.sh.day@gmail.com>
2024-06-05 17:47:02 +0900

    [2024-05-06] 쪽지 보관

---
+++ src/components/memo/Memo.js
@@ -0,0 +1,12 @@
+import "./Memo.css";
+import ReceiveMemoView from "./ReceiveMemoView";
+import ReceiveMemoApp from "./ReceiveMemoApp";
+
+export default function Memo() {
+  return (
+    <>
+      {/* <ReceiveMemoView /> */}
+      <ReceiveMemoApp />
+    </>
+  );
+}
--- src/components/memo/ReceiveMemoApp.js
+++ src/components/memo/ReceiveMemoApp.js
@@ -1,14 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import Checkbox from "../common/checkbox/Checkbox";
 import ReceiveMemoView from "./ReceiveMemoView";
+import { BsEnvelope, BsStar } from "react-icons/bs";
+import Table from "../../utils/Table";
+import { Radio } from "antd";
+import { Navigate } from "react-router-dom";
 
 let pageNo = 0;
 
-export default function MemoApp() {
+export default function ReceiveMemoApp() {
   const token = localStorage.getItem("token");
   const [data, setData] = useState({});
   const [needLoad, setNeedLoad] = useState();
   const [selectRcvMemoId, setSelectRcvMemoId] = useState();
+  const [selectionType, setSelectionType] = useState("checkbox");
 
   const isSelect = selectRcvMemoId !== undefined;
 
@@ -35,8 +40,87 @@ export default function MemoApp() {
   const { count, pages, next } = data || {};
   const { body: receiveMemos } = data || {};
 
+  console.log("receiveMemos >>", receiveMemos);
+
   const onRowClickHandler = async (rcvMemoId) => {
+    console.log(">>>>>>>>>>", rcvMemoId);
     setSelectRcvMemoId(rcvMemoId);
+  };
+
+  const columns = [
+    // {
+    //   title: "rcvMemoId",
+    //   dataIndex: "rcvMemoId",
+    //   key: "rcvMemoId",
+    //   width: "5%",
+    // },
+    {
+      title: <BsStar />,
+      dataIndex: "rcvSaveYn",
+      key: "rcvSaveYn",
+      width: "5%",
+    },
+    {
+      title: <BsEnvelope />,
+      dataIndex: "rcvDate",
+      key: "rcvDate",
+      width: "5%",
+    },
+    {
+      title: "발신일",
+      dataIndex: "rcvDate",
+      key: "rcvDate",
+      width: "20%",
+    },
+    {
+      title: "제목",
+      dataIndex: ["sendMemoVO", "memoTtl"],
+      key: "memoTtl",
+      render: (receiveMemos, row) => (
+        <span
+          style={{ cursor: "pointer" }}
+          onClick={() => onRowClickHandler(row.rcvMemoId)}
+        >
+          {receiveMemos}
+        </span>
+      ),
+    },
+    {
+      title: "수신일",
+      dataIndex: ["sendMemoVO", "sendDate"],
+      key: "sendDate",
+      width: "5%",
+    },
+  ];
+
+  const filterOptions = [
+    {
+      label: "사원명",
+      value: "empId",
+    },
+    {
+      label: "제목",
+      value: "memoTtl",
+    },
+    {
+      label: "내용",
+      value: "memoCntnt",
+    },
+  ];
+
+  const rowSelection = {
+    onChange: (selectedRowKeys, selectedRows) => {
+      console.log(
+        `selectedRowKeys: ${selectedRowKeys}`,
+        "selectedRows: ",
+        selectedRows
+      );
+    },
+    getCheckboxProps: (receiveMemos) => ({
+      disabled: receiveMemos.rcvMemoId === "Disabled User",
+      // Column configuration not to be checked
+      rcvMemoId: receiveMemos.rcvMemoId,
+    }),
   };
 
   return (
@@ -44,36 +128,18 @@ export default function MemoApp() {
       {token && !isSelect && (
         <>
           <div>받은 쪽지함: 총 {count}개</div>
-          <table>
-            <thead>
-              <tr>
-                <th>check</th>
-                <th>보관</th>
-                <th>읽음</th>
-                <th>읽음</th>
-                <th>제목</th>
-                <th>수신일</th>
-              </tr>
-            </thead>
-            <tbody>
-              {receiveMemos &&
-                receiveMemos.map((receiveMemo) => (
-                  <tr
-                    key={receiveMemo.rcvMemoId}
-                    onClick={() => onRowClickHandler(receiveMemo.rcvMemoId)}
-                  >
-                    <td>
-                      {/* <Checkbox id={receiveMemo.rcvMemoId} ref={checkboxRef} /> */}
-                    </td>
-                    <td>{receiveMemo.rcvSaveYn}</td>
-                    <td>{receiveMemo.rcvDate}</td>
-                    <td>{receiveMemo.sendMemoVO.memoTtl}</td>
-                    <td>{receiveMemo.sendMemoVO.memoCntnt}</td>
-                    <td>{receiveMemo.sendMemoVO.sendDate}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+
+          <Table
+            rowSelection={{
+              type: selectionType,
+              ...rowSelection,
+            }}
+            columns={columns}
+            dataSource={receiveMemos}
+            rowKey={(dt) => dt.rcvMemoId}
+            filter
+            filterOptions={filterOptions}
+          />
         </>
       )}
       {token && isSelect && (
