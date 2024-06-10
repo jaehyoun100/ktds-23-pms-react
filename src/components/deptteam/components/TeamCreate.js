0a82e6fdd4604ef2a3971f5498ad06f5b7e87c54
BumJin Jo <jobumjin5500@gmail.com>
2024-06-10 17:48:55 +0900

    [2024-06-10][조범진] 부서팀 수정

---
+++ src/components/deptteam/components/TeamCreate.js
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { createDepartment } from "../../../http/deptteamHttp";
+
+export default function TeamCreate({
+  setIsTeamRegistrationMode,
+  setNeedReload,
+  token,
+}) {
+  const [inputFields, setInputFields] = useState([
+    {
+      name: "",
+      empId: "",
+      deptId: "",
+    },
+  ]);
+
+  const handleInputChange = (index, event) => {
+    const values = [...inputFields];
+
+    values[index][event.target.name] = event.target.value;
+
+    setInputFields(values);
+  };
+
+  const handleAddFields = () => {
+    setInputFields([
+      ...inputFields,
+      {
+        name: "",
+        empId: "",
+        deptId: "",
+      },
+    ]);
+  };
+
+  const handleRemoveFields = (index) => {
+    const values = [...inputFields];
+    values.splice(index, 1);
+    setInputFields(values);
+  };
+
+  const onCancelClickHandler = () => {
+    setIsTeamRegistrationMode(false);
+  };
+
+  const onRegisterClickHandler = async () => {
+    for (const field of inputFields) {
+      const { name, empId, deptId } = field;
+      const json = await createDepartment(token, name, empId, deptId);
+
+      if (json.errors) {
+        json.errors.forEach((error) => {
+          alert(error);
+        });
+      } else if (json.body) {
+        setIsTeamRegistrationMode(false);
+        setNeedReload(Math.random());
+      }
+    }
+  };
+
+  return (
+    <>
+      {inputFields.map((inputField, index) => (
+        <div key={index}>
+          <div>
+            <label htmlFor={`name-${index}`}>팀명</label>
+            <input
+              type="text"
+              id={`name-${index}`}
+              name="name"
+              value={inputField.name}
+              onChange={(event) => handleInputChange(index, event)}
+            />
+          </div>
+          <div>
+            <label htmlFor={`deptId-${index}`}>소속 부서 ID</label>
+            <input
+              type="text"
+              id={`deptId-${index}`}
+              name="deptId"
+              value={inputField.deptId}
+              onChange={(event) => handleInputChange(index, event)}
+            />
+          </div>
+          <div>
+            <label htmlFor={`empId-${index}`}>팀장 ID</label>
+            <input
+              type="text"
+              id={`empId-${index}`}
+              name="empId"
+              value={inputField.empId}
+              onChange={(event) => handleInputChange(index, event)}
+            />
+          </div>
+
+          <div>
+            <button type="button" onClick={handleAddFields}>
+              +
+            </button>
+            {inputFields.length > 1 && (
+              <button type="button" onClick={() => handleRemoveFields(index)}>
+                -
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+      <div>
+        <button onClick={onRegisterClickHandler}>등록</button>
+        <button onClick={onCancelClickHandler}>취소</button>
+      </div>
+    </>
+  );
+}
--- src/http/deptteamHttp.js
+++ src/http/deptteamHttp.js
@@ -1,9 +1,30 @@
+const url =
+  "http://" +
+  (window.location.host === "43.202.29.221"
+    ? "43.202.29.221"
+    : "localhost:8080");
+
 export const loadDepartmentList = async ({ token, pageNo = 0 }) => {
   if (!token) {
     return undefined;
   }
+  const response = await fetch(`${url}/api/v1/department?pageNo=${pageNo}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const json = await response.json();
+
+  return json;
+};
+
+export const loadDepartmentDetail = async ({ token, selectedDeptId }) => {
+  if (!token) {
+    return undefined;
+  }
   const response = await fetch(
-    `http://localhost:8080/api/v1/department?pageNo=${pageNo}`,
+    `${url}/api/v1/department/detail/${selectedDeptId}`,
     {
       method: "GET",
       headers: {
@@ -20,15 +41,12 @@ export const loadTeamList = async ({ token, selectedDeptId }) => {
   if (!token) {
     return undefined;
   }
-  const response = await fetch(
-    `http://localhost:8080/api/v1/department/${selectedDeptId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: token,
-      },
-    }
-  );
+  const response = await fetch(`${url}/api/v1/department/${selectedDeptId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
   const json = await response.json();
 
   return json;
@@ -38,15 +56,45 @@ export const loadTeamMemberList = async ({ token, selectTmId }) => {
   if (!token) {
     return undefined;
   }
-  const response = await fetch(
-    `http://localhost:8080/api/v1/team/${selectTmId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: token,
-      },
-    }
-  );
+  const response = await fetch(`${url}/api/v1/team/${selectTmId}`, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  });
+  const json = await response.json();
+
+  return json;
+};
+
+export const createDepartment = async (token, name, empId) => {
+  const data = new FormData();
+  data.append("deptName", name);
+  data.append("deptLeadId", empId);
+
+  const response = await fetch(`${url}/api/v1/department`, {
+    method: "POST",
+    headers: { Authorization: token },
+    body: data,
+  });
+
+  const json = await response.json();
+
+  return json;
+};
+
+export const createTeam = async (token, name, empId, deptId) => {
+  const data = new FormData();
+  data.append("tmName", name);
+  data.append("tmLeadId", empId);
+  data.append("deptId", deptId);
+
+  const response = await fetch(`${url}/api/v1/team`, {
+    method: "POST",
+    headers: { Authorization: token },
+    body: data,
+  });
+
   const json = await response.json();
 
   return json;
