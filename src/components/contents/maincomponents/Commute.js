2f13550d5fdf3116b7a427e29d8c36df22086ca6
Seyoung Jung <syjung022722@gmail.com>
2024-06-17 16:27:30 +0900

    [2024-06-17] [정세영] 메인페이지 승인 디자인 변경

--- src/components/contents/maincomponents/Commute.js
+++ src/components/contents/maincomponents/Commute.js
@@ -1,13 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
 import w from "../ContentMain.module.css";
-import { FaDoorOpen } from "react-icons/fa";
+import { useEffect } from "react";
+import { getOneCommute } from "../../../http/commuteHttp";
+import Button from "../../common/Button/Button";
+import { logout } from "../../../http/userDetailHttp";
 
 export default function MainCommute() {
+  const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.tokenInfo);
+  const { commute } = useSelector((state) => state.commuteInfo);
+
+  useEffect(() => {
+    dispatch(getOneCommute(token));
+  }, [token, dispatch]);
+
+  const onLogoutBtnClickHandler = () => {
+    dispatch(logout(token, true));
+  };
   return (
     <>
       <div className={w.border}>
-        <div>출근 시간 (06-12 08:25)</div>
-        <div>퇴근 시간 (06-12 19:25)</div>
-        <button>퇴근</button>
+        <div>출근 시간 ({commute.cmmtTime})</div>
+        <div>퇴근 시간 ({commute.fnshTime})</div>
+        <Button onClickHandler={onLogoutBtnClickHandler}>퇴근</Button>
       </div>
     </>
   );
